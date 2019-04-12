894dacfdbd8107a93223b2ce9f5f1e086a6e87d6
dongxin <dongxin2019@gmail.com>
2019-04-12 17:04:54 +0800

    add header component

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-search">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+  .header
+    width 100%
+    display flex
+    line-height .86rem
+    color #fff
+    background-color $bgColor
+    .header-left
+      width .64rem
+      float left
+      .back-icon
+        text-align center
+        font-size .4rem
+    .header-search
+      flex 1
+      height .64rem
+      line-height .64rem
+      background-color #fff
+      padding-left .2rem
+      margin .12rem .1rem
+      border-radius .1rem
+      color #ccc
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      .arow-icon{
+        margin-left -.04rem
+        font-size .24rem
+      }
+</style>
