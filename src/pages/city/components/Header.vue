f163bb7f6938cd7e3a2b7eb87beb018c910a6353
dongxin <dongxin2019@gmail.com>
2019-04-13 22:13:50 +0800

    add city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont back-header">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+  .header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    font-size .32rem
+    background-color $bgColor
+    .back-header
+      position absolute
+      left 0
+      top 0
+      color #fff
+      font-size .3.4rem
+      font-weight bold
+      margin-left .2rem
+</style>
