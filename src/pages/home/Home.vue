00571da22b06cb822c4cf37dafd5cee4506a5fe4
dongxin <dongxin2019@gmail.com>
2019-04-16 16:55:08 +0800

    delete branch index-recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -10,32 +10,25 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
-import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
-import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommend,
     HomeWeekend
   },
   data () {
     return {
-      lastCity: '',
       swiperList: [],
       iconList: []
     }
   },
-  computed: {
-    ...mapState(['city'])
-  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json?city=' + this.city)
+      axios.get('/api/index.json')
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -49,12 +42,6 @@ export default {
   },
   mounted () {
     this.getHomeInfo()
-  },
-  activated () {
-    if (this.lastCity !== this.city) {
-      this.lastCity = this.city
-      this.getHomeInfo()
-    }
   }
 }
 </script>
